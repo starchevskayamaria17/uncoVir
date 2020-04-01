74a005666078e219052f1be4ac5bf8083757c08c
starchevskayamaria17 <44047649+starchevskayamaria17@users.noreply.github.com>
2020-04-01 17:26:58 +0700

    Update align_hg38.sh

--- Scripts/align_hg38.sh
+++ Scripts/align_hg38.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
+mkdir genomic_hg38
+cd genomic_hg38
+wget ftp://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/001/405/GCF_000001405.38_GRCh38.p12/GCF_000001405.38_GRCh38.p12_genomic.fna.gz
+gunzip GCF_000001405.38_GRCh38.p12_genomic.fna.gz
+bwa index GCF_000001405.38_GRCh38.p12_genomic.fna
+cd ../
+
 while read x
 do
-bwa mem -P {ПУТЬ ДО ДИРЕКТОРИИ hg38}/GCF_000001405.38_GRCh38.p12_genomic.fna ${x}_1.paired.fastq ${x}_2.paired.fastq > align_hg38_${x}.sam
+bwa mem -P genomic_hg38/GCF_000001405.38_GRCh38.p12_genomic.fna ${x}_1.paired.fastq ${x}_2.paired.fastq > align_hg38_${x}.sam
 done<$1
