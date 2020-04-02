d6c40d98774b9580ebfc4bc4799bbc958f8e7e6d
starchevskayamaria17 <44047649+starchevskayamaria17@users.noreply.github.com>
2020-04-02 11:02:02 +0700

    Update repair.sh

--- Scripts/repair.sh
+++ Scripts/repair.sh
@@ -2,7 +2,7 @@
 
 while read x
 do
-{ПУТЬ ДО ДИРЕКТОРИИ С BBMAP}/bbmap/repair.sh in=${x}_1.fastq in2=${x}_2.fastq out=${x}_1.paired.fastq out2=${x}_2.paired.fastq outs=${x}.unpaired.fastq
+/bbmap/repair.sh in=${x}_1.fastq in2=${x}_2.fastq out=${x}_1.paired.fastq out2=${x}_2.paired.fastq outs=${x}.unpaired.fastq
 mv ${x}_1.pair.fastq ${x}_1.fastq
 mv ${x}_2.pair.fastq ${x}_2.fastq
 done<$1
